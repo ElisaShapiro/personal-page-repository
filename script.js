b5fee73f48f6d3d3dc165510275a21ad3a503bca
Elisa Shapiro <elisashapiro@gmail.com>
2021-06-17 18:09:33 -0400

    recent updates to all

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This is added by JS";
-document.querySelector('body').appendChild(h2);
-
-
-const buttonElement = document.getElementById('btn');
-buttonElement.addEventListener('click', function() {
-    alert ('Moose was adopted from Franciss Friends Inc in Astoria');
-});
+function myFunction() {
+    document.getElementById("btnInfo").innerHTML = "Moose was adopted from Franciss Friends Inc in Astoria";
+}
 
