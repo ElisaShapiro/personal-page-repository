613f2b7c4556bac440076c6c47ecf4f2ddcf3524
Elisa Shapiro <elisashapiro@gmail.com>
2021-06-17 14:35:25 -0400

    Added some more styling, updating all pages

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h2 = document.createElement('h2');
 h2.textContent = "This is added by JS";
 document.querySelector('body').appendChild(h2);
+
+function myFunction() {
+    document.getElementById("mooseEvent").innerHTML = "Moose was adopted from Franciss Friends Inc in Astoria"
+}
+
