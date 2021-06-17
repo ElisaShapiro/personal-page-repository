fcc612b30f418a3d0a766f40a00de90ab2958cd4
Elisa Shapiro <elisashapiro@gmail.com>
2021-06-17 16:45:32 -0400

    added image hopefully for real, added js

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const h2 = document.createElement('h2');
 h2.textContent = "This is added by JS";
 document.querySelector('body').appendChild(h2);
 
-function myFunction() {
-    document.getElementById("mooseEvent").innerHTML = "Moose was adopted from Franciss Friends Inc in Astoria"
-}
+
+const buttonElement = document.getElementById('btn');
+buttonElement.addEventListener('click', function() {
+    alert ('Moose was adopted from Franciss Friends Inc in Astoria');
+});
 
